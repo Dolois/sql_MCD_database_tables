fad4cda83e5a9ddb0a1db25e8bd83a400de7fa17
Dolois <39958651+Dolois@users.noreply.github.com>
2018-08-12 12:48:20 +0200

    Update create table niveau_parking.sql

--- create table niveau_parking.sql
+++ create table niveau_parking.sql
@@ -1,7 +1,7 @@
 USE batiment;
 
 CREATE TABLE niveau_parking (
-    -- l'attribut niveau_parking aura des valeurs de type INT
+	-- l'attribut niveau_parking aura des valeurs de type INT
 	-- et ne pourra pas être à NULL
 	-- et s'auto-incrémente
 	niveau_parking INT NOT NULL AUTO_INCREMENT,
@@ -10,6 +10,6 @@ CREATE TABLE niveau_parking (
 	-- la clé primaire de cette entité est l'attribut niveau_parking 
 	PRIMARY KEY ( niveau_parking )
 	-- La clé étrangère de cette table sera l'attribut num_batiment
-    -- Il aura pour référence l'attribut id_batiment de la table batiment
+	-- Il aura pour référence l'attribut id_batiment de la table batiment
 	FOREIGN KEY num_batiment REFERENCES batiment( id_batiment);
 );
