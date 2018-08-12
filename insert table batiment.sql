09d11e5ea12c0eb079a076e39cff02bd9707a5ff
Dolois <39958651+Dolois@users.noreply.github.com>
2018-08-12 19:11:32 +0200

    Add files via upload

---
+++ insert table batiment.sql
@@ -0,0 +1,12 @@
+INSERT INTO batiment (adresse, nbr_etage, nbr_niveau_parking)
+VALUES
+('7 rue du chemin vert 75011 Paris', 5, 3),
+('15 rue des merles 75012 Paris', 7, 2),
+('154 boulevard martin 75005 Paris', 6, 1),
+('45 villa stendhal 75020 Paris', 2, 1),
+('322 rue saint germain 92220 Boulogne', 4, 2),
+('1425 boulevard lenoir 75011 Paris', 11, 2),
+('733 boulevard saint denis 75002 Paris', 10, 5),
+('5 ter rue du moulin 76600 Fécamp', 10, 5),
+('2 bis impasse des mouettes 14000 Caen', 5, 2),
+('33 rue de bordeaux 22200 Paimpol', 7, 3);
