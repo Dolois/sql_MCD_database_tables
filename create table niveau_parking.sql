1c256126e65184dc44b53e9d8b6284d355353539
Dolois <39958651+Dolois@users.noreply.github.com>
2018-08-12 12:47:02 +0200

    Add files via upload

---
+++ create table niveau_parking.sql
@@ -0,0 +1,15 @@
+USE batiment;
+
+CREATE TABLE niveau_parking (
+    -- l'attribut niveau_parking aura des valeurs de type int
+	-- et ne pourra pas être à NULL
+	-- et s'auto-incrémente
+	niveau_parking INT NOT NULL AUTO_INCREMENT,
+	nbr_place INT,
+	num_batiment INT,
+	-- la clé primaire de cette entité est l'attribut niveau_parking 
+	PRIMARY KEY ( niveau_parking )
+	-- La clé étrangère de cette table sera l'attribut num_batiment
+    -- Il aura pour référence l'attribut id_batiment de la table batiment
+	FOREIGN KEY num_batiment REFERENCES batiment( id_batiment);
+);
