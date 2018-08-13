931c54cb14c1e5934753cf8d7ad6ae4adfa184c8
Dolois <39958651+Dolois@users.noreply.github.com>
2018-08-13 16:04:18 +0200

    Delete create table batiment.sql

--- create table batiment.sql
+++
@@ -1,9 +0,0 @@
-USE batiment;
-
-CREATE TABLE `batiment` (
-	`id_batiment` INT NOT NULL AUTO_INCREMENT,
-	`adresse` VARCHAR(60),
-	`nbr_etage` INT,
-	`nbr_niveau_parking` INT,
-	PRIMARY KEY ( `id_batiment` )
-);
